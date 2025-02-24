78a6b739de771885a9b8cc1ef63e735b3b471f50
ctgmovies198 <ctgmovies198@gmail.com>
2025-02-25 02:36:16 +0600

    Update info.py

--- info.py
+++ info.py
@@ -1,16 +1,16 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @TMSLZOON
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@teraboxslinks?si=B0QgOcm73NjPXK4O
+# Ask Doubt on telegram @TMSLZOON
 
 # Never Give DATABASE_URI And BOT_TOKEN Here If Repo Is Forked Or Public Because This May Lead Account Ban.
 
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", ""))
-API_HASH     = environ.get("API_HASH", "")
+API_ID       = int(environ.get("API_ID", "28148293"))
+API_HASH     = environ.get("API_HASH", "32ce81ffdd4684a856370eccf62b1f80")
 BOT_TOKEN    = environ.get("BOT_TOKEN", "")
-DATABASE_URI = environ.get("DATABASE_URI", "")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
-ADMIN        = int(environ.get("ADMIN", ""))
-CHANNEL      = environ.get("CHANNEL", "@VJ_Botz")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://bahegek318:n6rrzEYVVJGE9g21@cluster0.bbmdq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002381954054"))
+ADMIN        = int(environ.get("ADMIN", "7561827913"))
+CHANNEL      = environ.get("CHANNEL", "@TMSLZOONOFFICIAL")
