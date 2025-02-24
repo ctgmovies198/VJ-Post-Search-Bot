b5b06fe077adfcea5c767b0510f4de3f8bbced9f
ctgmovies198 <ctgmovies198@gmail.com>
2025-02-25 02:37:08 +0600

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @TMSLZOON
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@teraboxslinks?si=B0QgOcm73NjPXK4O
+# Ask Doubt on telegram @TMSLZOON
 
 from info import *
 from pyrogram import Client
@@ -15,6 +15,6 @@ class Bot(Client):
             plugins={"root": "plugins"})
     async def start(self):                        
         await super().start()  
-        print("Bot Started 🔧 Powered By @VJ_Botz")   
+        print("Bot Started 🔧 Powered By @TMSLZOON")   
     async def stop(self, *args):
         await super().stop()
