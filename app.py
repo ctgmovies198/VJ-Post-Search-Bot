0b69f49bfe6424e29fd9237760c39f1e06187641
ctgmovies198 <ctgmovies198@gmail.com>
2025-02-25 02:37:47 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @TMSLZOON
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@teraboxslinks?si=B0QgOcm73NjPXK4O
+# Ask Doubt on telegram @TMSLZOON
 
 from flask import Flask
 app = Flask(__name__)
