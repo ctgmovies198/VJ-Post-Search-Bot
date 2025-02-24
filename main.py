bd2ce78e827e6755fb25bccd7d00f4f8f9929cfc
ctgmovies198 <ctgmovies198@gmail.com>
2025-02-25 02:33:07 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @TMSLZOON
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@teraboxslinks?si=B0QgOcm73NjPXK4O
+# Ask Doubt on telegram @TMSLZOON
 
 from client import Bot
 
-print("Bot Started 👍 Powered By @VJ_Botz")
+print("Bot Started 👍 Powered By @TMSLZOON")
 Bot().run()
 
